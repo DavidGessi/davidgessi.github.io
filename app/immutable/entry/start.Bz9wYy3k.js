492e1b3865b3ba0e8bfa20f13d5c9ba3f0e60fc0
gessw <gesswagnerd@gmail.com>
2026-06-15 18:29:14 +0200

    Updates

--- app/immutable/entry/start.Bz9wYy3k.js
+++
@@ -1,1 +0,0 @@
-import{n as e,s as t}from"../chunks/C_LYz7hu.js";export{t as load_css,e as start};
---
+++ app/immutable/entry/start.CCSBwlpk.js
@@ -0,0 +1,1 @@
+import{n as e,s as t}from"../chunks/CrSk6I1d.js";export{t as load_css,e as start};
